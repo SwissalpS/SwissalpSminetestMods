ea7c8b97454f4d995f6d5c4656b4d79d86e4be13
SwissalpS <Luke@SwissalpS.ws>
2016-03-15 00:28:34 +0100

    it's clearing out the full list

--- swissalps_teleport/pad.lua
+++ swissalps_teleport/pad.lua
@@ -425,8 +425,11 @@ function SssStpP.onFieldsAdvanced(tPos, tFields, sPlayer)
 		local iIndex = SssStpP.cacheGet(sPlayer, 'iIndexBookmarkB', 1);
 		sListB = SssStpP.cacheGet(sPlayer, 'sListB', '');
 		local aListB = string.split(sListB, ',');
+		print(iIndex, dump(aListB));
 		aListB[iIndex] = nil;
+		print(dump(aListB));
 		sListB = table.implodeStrings(aListB, ',');
+		print('listB after:', sListB);
 		SssStpP.cachePut(sPlayer, 'sListB', sListB);
 	end; -- if remove from list B
 	if bApplySelected then
